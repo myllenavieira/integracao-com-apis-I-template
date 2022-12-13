a1eb5974bccdce1c13d8489cc66fd448915fd074
myllenavieira <vieirasilvamyllena@gmail.com>
2022-12-12 21:49:00 -0300

    Exercícios

--- src/Componentes/AddUsuario/AddUsuario.js
+++ src/Componentes/AddUsuario/AddUsuario.js
@@ -1,8 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function AddUsuario(props) {
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
+
+  const createUser = () => {
+
+    const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
+    const config = { headers: { Authorization: "myllena-vieira-barbosa" } };
+    const body = {
+      "name": nome,
+      "email": email
+    };
+
+    axios
+      .post(url, body, config)
+      .then((res) => { alert("Usuário criado com sucesso!") })
+      .catch((err) => { console.log(err); })
+  }
 
   return (
     <>
@@ -22,7 +38,7 @@ function AddUsuario(props) {
           setEmail(e.target.value);
         }}
       />
-      <button>Enviar</button>
+      <button onClick={() => { createUser() }}>Enviar</button>
     </>
   );
 }
--- src/Componentes/Usuario/Usuario.js
+++ src/Componentes/Usuario/Usuario.js
@@ -1,16 +1,65 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import axios from "axios"
 
 const User = styled.div`
   border: black 1px solid;
   margin-top: 10px;
-  width: 350px
+  width: 350px;
 `
 function Usuario(props) {
   const [usuario, setUsuario] = useState(props.usuario);
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [editar, setEditar] = useState(false);
+
+  const getUsersById = (id) => {
+    /* const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`
+    const config = { headers: { Authorization: "myllena-vieira-barbosa" } } */
+
+    axios
+      .get(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`, { headers: { Authorization: "myllena-vieira-barbosa" } })
+      .then((res) => setUsuario(res.data))
+      .catch((err) => { console.log(err) })
+  }
+
+  useEffect(() => {
+    getUsersById(props.usuario.id)
+  }, [])
+
+  const editUser = (id) => {
+
+    const body = {
+      "name": nome,
+      "email": email
+    }
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`
+    const config = { headers: { Authorization: "myllena-vieira-barbosa" } }
+
+    axios.put(url, body, config)
+      .then((res) => {
+        alert("Usuário editado com sucesso")
+        setEditar(!editar)
+      }).catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const deleteUsers = (id) => {
+
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id} `
+    const config = { headers: { Authorization: "myllena-vieira-barbosa" } }
+
+    axios
+      .delete(url, config)
+      .then((res) => {
+        alert("usuario Deletado com sucesso")
+      })
+      .catch((err) => {
+        alert(err.response.data.message)
+
+      })
+  }
 
   return (
     <User>
@@ -20,7 +69,7 @@ function Usuario(props) {
           <p>E-mail:{usuario.email}</p>
           <input value={nome} onChange={(e) => setNome(e.target.value)} />
           <input value={email} onChange={(e) => setEmail(e.target.value)} />
-          <button>Enviar alterações</button>
+          <button onClick={() => { editUser(props.usuario.id) }}>Enviar alterações</button>
         </div>
       ) : (
         <>
@@ -29,7 +78,7 @@ function Usuario(props) {
         </>
       )}
       <button onClick={() => setEditar(!editar)}>Editar</button>
-      <button>Excluir</button>
+      <button onClick={() => { deleteUsers(props.usuario.id) }}>Excluir</button>
     </User>
   );
 }
